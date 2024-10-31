ab27f247dca03f782121141057f1f5ae6436e2b3
Olivier Hebert <heebertolivier@gmail.com>
2024-10-31 10:34:20 -0400

    premier commit

---
+++ exercice11/js/app.js
@@ -0,0 +1,17 @@
+"use strict"
+let boutonJeuxVideos = document.getElementById ("progJeux");
+boutonJeuxVideos.addEventListener('click', function(){
+alert("J'aime le cours Programmation Web et jeux vidéo")
+});
+let boutonWebDynamique = document.getElementById ("progDynamique");
+boutonWebDynamique.addEventListener('click', function(){
+alert("J'aime le cours Programmation Web et jeux vidéo")
+});
+let boutonAppWeb = document.getElementById ("devAppWeb");
+boutonAppWeb.addEventListener('click', function(){
+alert("J'aime le cours Programmation Web et jeux vidéo")
+});
+let boutonServicesWeb = document.getElementById ("devServicesWeb");
+boutonServicesWeb.addEventListener('click', function(){
+alert("J'aime le cours Programmation Web et jeux vidéo")
+});
